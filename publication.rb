6ab4af09dd128df09b008d299b5789e0c4ea311f
Andy Buntine <bunts@hhd.com.au>
2013-08-05 10:35:35 +1000

    Got currencies rendering

--- publication.rb
+++ publication.rb
@@ -1,11 +1,16 @@
 require "sinatra"
+require "simple_xurrency"
 require "active_support/core_ext/integer/inflections"
 
-supported_curr= ["eur", "usd", "gbp", "aud", "brl", "cad", "chf", "cny", "dkk", "hkd", "inr", "jpy", "krw",
-                 "lkr", "myr", "nzd", "sgd", "twd", "zar", "thb", "sek", "nok", "mxn", "bgn", "czk", "huf",
-                 "ltl", "lvl", "pln", "ron", "isk", "hrk", "rub", "try", "php", "cop", "ars", "clp", "svc",
-                 "tnd", "pyg", "mad", "jmd", "sar", "qar", "hnl", "syp", "kwd", "bhd", "egp", "omr", "ngn",
-                 "pab", "pen", "uyu"]
+supported_curr = [:eur, :usd, :gbp, :aud, :brl, :cad, :chf, :cny, :dkk, :hkd, :inr, :jpy, :krw,
+                 :lkr, :myr, :nzd, :sgd, :twd, :zar, :thb, :sek, :nok, :mxn, :bgn, :czk, :huf,
+                 :ltl, :lvl, :pln, :ron, :isk, :hrk, :rub, :try, :php, :cop, :ars, :clp, :svc,
+                 :tnd, :pyg, :mad, :jmd, :sar, :qar, :hnl, :syp, :kwd, :bhd, :egp, :omr, :ngn,
+                 :pab, :pen, :uyu]
+
+popular_curr = [:eur, :usd, :gbp, :aud, :brl, :cad, :chf, :cny, :dkk, :hkd, :inr, :jpy, :krw]
+
+SimpleXurrency.key="a68f78dfde1be099be24543b7096a838"
 
 get "/edition/" do
   # Fetch preferred currency
@@ -15,12 +20,27 @@ get "/edition/" do
     # Get rate and inverse
   # eTag with updated_at from Xurrency
 
-  etag Digest::MD5.hexdigest("edition")
+  currency = params[:currency]
+  @rates = []
+
+  if supported_curr.include?(currency.to_sym)
+    popular_curr.each do |pc|
+      unless pc.to_s == currency.to_s
+        @rates << [1.send(currency).send("to_#{pc}"),
+                   1.send(pc).send("to_#{currency}"),
+                   1.send(currency).send("to_#{pc}_updated_at")]
+      end
+    end
+  end
+
+  content_type "text/html; charset=utf-8"
+  etag Digest::MD5.hexdigest(Time.now.to_i.to_s)
   erb :rates
 end
 
 get "/sample/" do
-  etag Digest::MD5.hexdigest("sample")
+  content_type "text/html; charset=utf-8"
+  etag Digest::MD5.hexdigest(Time.now.to_i.to_s)
   erb :rates
 end
 
