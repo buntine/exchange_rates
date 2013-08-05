b8701e6908c423b2dc1eeda90db4e58aecc0ded5
Andy Buntine <bunts@hhd.com.au>
2013-08-05 16:53:27 +1000

    Added sample

--- publication.rb
+++ publication.rb
@@ -3,45 +3,62 @@ require "date"
 require "simple_xurrency"
 require "active_support/core_ext/integer/inflections"
 
-supported_curr = [:eur, :usd, :gbp, :aud, :brl, :cad, :chf, :cny, :dkk, :hkd, :inr, :jpy, :krw,
+$supported_curr = [:eur, :usd, :gbp, :aud, :brl, :cad, :chf, :cny, :dkk, :hkd, :inr, :jpy, :krw,
                  :lkr, :myr, :nzd, :sgd, :twd, :zar, :thb, :sek, :nok, :mxn, :bgn, :czk, :huf,
                  :ltl, :lvl, :pln, :ron, :isk, :hrk, :rub, :try, :php, :cop, :ars, :clp, :svc,
                  :tnd, :pyg, :mad, :jmd, :sar, :qar, :hnl, :syp, :kwd, :bhd, :egp, :omr, :ngn,
                  :pab, :pen, :uyu]
 
-popular_curr = [:eur, :usd, :gbp, :aud, :brl, :cad, :chf, :cny, :dkk, :hkd, :inr, :jpy]
+$popular_curr = [:eur, :usd, :gbp, :aud, :brl, :cad, :chf, :cny, :dkk, :hkd, :inr, :jpy]
 
 SimpleXurrency.key = "a68f78dfde1be099be24543b7096a838"
 
-get "/edition/" do
-  return 400, "Error: No local_delivery_time was provided" if params[:local_delivery_time].nil?
-  return 400, "Error: No currency was provided" if params[:currency].nil?
-
-  @currency = params[:currency]
+helpers do
+def build_rates(curr)
+  @currency = curr
   @rates = []
 
-  if supported_curr.include?(@currency.to_sym)
-    popular_curr.each do |pc|
+  if $supported_curr.include?(@currency.to_sym)
+    $popular_curr.each do |pc|
       unless pc.to_s == @currency.to_s
         rate = 1.send(@currency).send("to_#{pc}")
         @rates << [pc, rate, (1.0 / rate).round(4)]
       end
     end
 
-    @local_time = Time.parse(params[:local_delivery_time])
-    @updated_at = Time.parse(1.send(@currency).send("to_#{popular_curr.first}_updated_at")) - (60 * 60)
-  else
+    @local_time = if params[:local_delivery_time]
+      Time.parse(params[:local_delivery_time])
+    else
+      Time.now
+    end
+
+    @updated_at = Time.parse(1.send(@currency).send("to_#{$popular_curr.first}_updated_at")) - (60 * 60)
+
+    true
+  end
+end
+end
+
+get "/edition/" do
+  return 400, "Error: No local_delivery_time was provided" if params[:local_delivery_time].nil?
+  return 400, "Error: No currency was provided" if params[:currency].nil?
+
+  if not build_rates(params[:currency])
     return 400, "Error: Invalid currency was provided"
   end
 
   content_type "text/html; charset=utf-8"
-  etag Digest::MD5.hexdigest(@updated_at.to_s)
+  etag Digest::MD5.hexdigest(@currency + @updated_at.to_s)
   erb :rates
 end
 
 get "/sample/" do
+  if not build_rates("usd")
+    return 400, "Error: Invalid currency was provided"
+  end
+
   content_type "text/html; charset=utf-8"
-  etag Digest::MD5.hexdigest(Time.now.to_i.to_s)
+  etag Digest::MD5.hexdigest(@currency + @updated_at.to_s)
   erb :rates
 end
 
@@ -61,7 +78,7 @@ get "/validate_config/" do
     response[:errors].push("Please select a currency from the select box.")
   end
   
-  unless supported_curr.include?(user_settings["currency"].downcase)
+  unless $supported_curr.include?(user_settings["currency"].downcase)
     response[:valid] = false
     response[:errors].push("We couldn't find the currency you selected (#{user_settings["currency"]}). Please select another.")
   end
